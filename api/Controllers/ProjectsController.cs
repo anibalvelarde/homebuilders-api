71e563574ec02c0af99782cbde99ec796ded4fcf
AEV0129 <AEV0129@daugherty.com>
2021-05-16 08:23:09 -0500

    Added XML comments to the Controller types

--- api/Controllers/ProjectsController.cs
+++ api/Controllers/ProjectsController.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Logging;
 
 namespace HomeBuilders.Api.Controllers
 {
+    /// <summary>
+    /// Handles management of Project resources
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProjectsController
@@ -15,12 +18,21 @@ namespace HomeBuilders.Api.Controllers
         private ILogger<ProjectsController> _logger;
         private IProjectsService _service;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">Instance of the Logger service</param>
+        /// <param name="service">Instance of the Project Management service</param>
         public ProjectsController(ILogger<ProjectsController> logger, IProjectsService service)
         {
             _logger = logger;
             _service = service;
         }
 
+        /// <summary>
+        /// Gets a list of Project resources
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("/projects")]
         public async Task<List<Project>> GetProjectsAsync()
@@ -28,6 +40,11 @@ namespace HomeBuilders.Api.Controllers
             return await _service.GetProjectsAsync();
         }
 
+        /// <summary>
+        /// Get a single Project resource
+        /// </summary>
+        /// <param name="id">Unique identifier for a Project resource</param>
+        /// <returns>A Project resource instance</returns>
         [HttpGet]
         [Route("/projects/{id}")]
         public async Task<Project> GetProjectById(int id)
@@ -35,6 +52,11 @@ namespace HomeBuilders.Api.Controllers
             return await _service.GetProjectByIdAsync(id);
         }
 
+        /// <summary>
+        /// Creates a new Project resource in the platform
+        /// </summary>
+        /// <param name="ProjectRequest">Well-formed info set to properly create a Project resource</param>
+        /// <returns>The Project resource that was added to the platform</returns>
         [HttpPost]
         [Route("/projects")]
         public async Task<Project> AddNewProject([FromBody] NewProjectRequest ProjectRequest)
@@ -47,6 +69,11 @@ namespace HomeBuilders.Api.Controllers
             );
         }
 
+        /// <summary>
+        /// Updates an existing Project resource
+        /// </summary>
+        /// <param name="ProjectRequest">Well-formed info set to properly update a Project resource</param>
+        /// <returns></returns>
         [HttpPut]
         [Route("/projects")]
         public async Task<Project> UpdateExistingProject([FromBody] ExistingProjectRequest ProjectRequest)
--- api/Controllers/ServicePlansController.cs
+++ api/Controllers/ServicePlansController.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Logging;
 
 namespace HomeBuilders.Api.Controllers
 {
+    /// <summary>
+    /// Handles management of ServicePlan resources
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ServicePlansController
@@ -15,19 +18,34 @@ namespace HomeBuilders.Api.Controllers
         private ILogger<ServicePlansController> _logger;
         private IServicePlansService _service;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">A logger service</param>
+        /// <param name="service">A ServicePlan Manager service</param>
         public ServicePlansController(ILogger<ServicePlansController> logger, IServicePlansService service)
         {
             _logger = logger;
             _service = service;
         }
 
+        /// <summary>
+        /// Gets a list of all Service Plans in the platform
+        /// </summary>
+        /// <param name="pageItemCount">Max items returned in a single request. Pagination is required to extract other items></param>
+        /// <returns>The next n (where n = pageItemCount) items in the list of Service Plan resources</returns>
         [HttpGet]
         [Route("/ServicePlans")]
-        public async Task<List<ServicePlan>> GetServicePlansAsync()
+        public async Task<List<ServicePlan>> GetServicePlansAsync(int pageItemCount = 15)
         {
             return await _service.GetServicePlansAsync();
         }
 
+        /// <summary>
+        /// Gets a single ServicePlan resource
+        /// </summary>
+        /// <param name="id">Unique identifier for a Service Plan resource</param>
+        /// <returns>An instance of ServicePlan resource</returns>
         [HttpGet]
         [Route("/ServicePlans/{id}")]
         public async Task<ServicePlan> GetServicePlanById(int id)
@@ -35,6 +53,11 @@ namespace HomeBuilders.Api.Controllers
             return await _service.GetServicePlanByIdAsync(id);
         }
 
+        /// <summary>
+        /// Creates a new Service Plan resource
+        /// </summary>
+        /// <param name="ServicePlanRequest">Well-formed info to create a Service Plan resource</param>
+        /// <returns>The ServicePlan resource that was added to the platform</returns>
         [HttpPost]
         [Route("/ServicePlans")]
         public async Task<ServicePlan> AddNewServicePlan([FromBody] NewServicePlanRequest ServicePlanRequest)
@@ -42,6 +65,11 @@ namespace HomeBuilders.Api.Controllers
             return await _service.AddNewServicePlanAsync(ServicePlanRequest.NewPlan);
         }
 
+        /// <summary>
+        /// Updates an existing Service Plan resource
+        /// </summary>
+        /// <param name="ServicePlanRequest">Well-formed info to update an existing Service Plan resource</param>
+        /// <returns>The Service Plan resource that was updated</returns>
         [HttpPut]
         [Route("/ServicePlans")]
         public async Task<ServicePlan> UpdateExistingServicePlan([FromBody] ExistingServicePlanRequest ServicePlanRequest)
